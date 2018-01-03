638f96d387c2b4d478f1cfdfa9b7f3f3abe6b70c
rychukarter <xlulus3@gmail.com>
2018-01-03 17:47:27 +0100

    test feature added

--- process_raw.py
+++ process_raw.py
@@ -14,9 +14,17 @@ for file in raw_files:
     raw_data_df = pd.read_excel(my_path + file, index_col=0)
     raw_data_df = raw_data_df.iloc[3:]
     raw_data_df.index = pd.to_datetime(raw_data_df.index)
+    for index, row in raw_data_df.iterrows():
+        print(index.date())
+        #print(row)
+        #print(row["Pm.a06a"])
+        try:
+            df.at[index.date(),"PM10_h_" + str(index.hour)] = row["MzWarNiepodKom"]
+        except:
+            print("NOT ADDED:", index.date())
 
-    print(raw_data_df.columns)
-
+print(df)
+df.to_csv("test.csv", sep=";")
 
 
 #df.to_csv("data4.csv", sep=";")
